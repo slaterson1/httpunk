568e9092bcb27c48c4794091f2e28dd20a60c36c
jbudrow <jbudrowd3@yahoo.com>
2016-03-15 13:58:49 -0400

    created module, api class

---
+++ lib/server.rb
@@ -0,0 +1,14 @@
+require "sinatra/base"
+require "json"
+require "pry"
+
+module Httpunk
+  class Api < Sinatra::Base
+    get "/api/test" do
+      content_type :json
+      {message: "Go go go!"}.to_json
+    end
+    
+    run! if app_file == $0
+  end
+end
